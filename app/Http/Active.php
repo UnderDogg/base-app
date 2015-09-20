80f05db9125389b545dfb20d977bded80bd0b421
Steve Bauman <steven_bauman@outlook.com>
2015-09-20 13:50:25 -0400

    Added Active helper class

---
+++ app/Http/Active.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Support\Facades\Route;
+
+class Active
+{
+    /**
+     * The wildcard string.
+     *
+     * @var string
+     */
+    protected $wildcard = '*';
+
+    /**
+     * The output class string.
+     *
+     * @var string
+     */
+    protected $output = 'active';
+
+    /**
+     * The current route name.
+     *
+     * @var string
+     */
+    protected $route = '';
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->route = Route::currentRouteName();
+    }
+
+    /**
+     * Returns the output if the specified route
+     * is the current route.
+     *
+     * @param string $route
+     *
+     * @return null|string
+     */
+    public function route($route)
+    {
+        $current = $this->route;
+
+        if ($this->containsWildcard($route)) {
+            $route = $this->stripWildcard($route);
+
+            return $this->route($route);
+        } else if (str_contains($current, $route)) {
+            return $this->output;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the output if one of the specified
+     * routes are the current route.
+     *
+     * @param array $routes
+     * @param string $output
+     *
+     * @return null|string
+     */
+    public function routes(array $routes = [], $output = '')
+    {
+        foreach ($routes as $route) {
+            $output = $this->route($route, $output);
+
+            if ($output) return $output;
+        }
+
+        return null;
+    }
+
+    /**
+     * Sets the output class string.
+     *
+     * @param string $output
+     *
+     * @return Active
+     */
+    public function output($output)
+    {
+        $this->output = $output;
+
+        return $this;
+    }
+
+    /**
+     * Returns true / false if the specified
+     * string contains a wildcard.
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+    protected function containsWildcard($string)
+    {
+        return str_contains($string, $this->wildcard);
+    }
+
+    /**
+     * Removes the wildcard from the specified string.
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    protected function stripWildcard($string)
+    {
+        return str_replace($this->wildcard, null, $string);
+    }
+}
