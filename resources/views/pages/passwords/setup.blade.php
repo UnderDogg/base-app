12d37a6f160c32a0f2ec736b9397eef92c6c2698
Steve Bauman <steven_bauman@outlook.com>
2015-09-23 17:24:07 -0400

    Small view changes

--- resources/views/pages/passwords/setup.blade.php
+++ resources/views/pages/passwords/setup.blade.php
@@ -2,12 +2,26 @@
 
 @section('title', 'Setup Passwords')
 
+@section('title.header')
+    <h3 class="text-center">
+        @yield('title')
+    </h3>
+@stop
+
 @section('content')
 
     <div class="col-md-3"></div>
 
     <div class="col-md-6 text-center">
         <i class="fa fa-6 fa-lock"></i>
+
+        <div class="alert alert-info" role="alert">
+            <b>Hey There.</b>
+
+            <br>
+
+            You have to setup a pin before you can start storing passwords.
+        </div>
     </div>
 
     <div class="col-md-3"></div>
