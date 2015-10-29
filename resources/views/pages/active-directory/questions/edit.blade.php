0bdd613e79e80d639563119010a7bafd3e50fc6d
Steve Bauman <steven_bauman@outlook.com>
2015-10-29 16:44:33 -0400

    Added delete button to edit question view

--- resources/views/pages/active-directory/questions/edit.blade.php
+++ resources/views/pages/active-directory/questions/edit.blade.php
@@ -6,4 +6,21 @@
 
     {!! $form !!}
 
+    <hr>
+
+    <div class="col-md-12 text-center">
+
+        <a
+                class="btn btn-danger"
+                data-post="DELETE"
+                data-title="Delete Question?"
+                data-message="Are you sure you want to delete this question? Users using this question will need to re-add another question after deletion."
+                href="{{ route('active-directory.questions.destroy', [$question->getKey()]) }}">
+            Delete
+
+            <i class="fa fa-times"></i>
+        </a>
+
+    </div>
+
 @stop
