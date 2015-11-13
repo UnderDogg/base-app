a82d1dc98e73a1e8df3de7c079d20302dee83df5
Steve Bauman <steven_bauman@outlook.com>
2015-11-13 14:58:40 -0500

    Remove unneeded classes

--- tests/WelcomeTest.php
+++ tests/WelcomeTest.php
@@ -2,10 +2,6 @@
 
 namespace App\Tests;
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
 class WelcomeTest extends TestCase
 {
     /**
