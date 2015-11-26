721a2df3038798cfd769870121c9c09c19cc0d1c
Steve Bauman <steven_bauman@outlook.com>
2015-11-26 14:37:12 -0500

    Catch ldap bind errors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,8 +62,12 @@ class AuthController extends Controller
 
         $credentials = $this->getCredentials($request);
 
-        if (Auth::attempt($credentials, $request->has('remember'))) {
-            return $this->handleUserWasAuthenticated($request, $throttles);
+        try {
+            if (Auth::attempt($credentials, $request->has('remember'))) {
+                return $this->handleUserWasAuthenticated($request, $throttles);
+            }
+        } catch (\Exception $e) {
+           // Catch LDAP bind errors.
         }
 
         // If the login attempt was unsuccessful we will increment the number of attempts
