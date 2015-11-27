f779201a5bf4b99d9a666a9dddb1fbd46c4601a4
Steve Bauman <steven_bauman@outlook.com>
2015-11-27 10:21:03 -0500

    Clean up some admin-lte theme views

---
+++ public/themes/adminlte/packages/orchestra/control/home.blade.php
@@ -0,0 +1,5 @@
+@extends('orchestra/foundation::layouts.page')
+
+@section('content')
+    @include('orchestra/control::widgets.header')
+@stop
--- public/themes/adminlte/packages/orchestra/foundation/dashboard/index.blade.php
+++ public/themes/adminlte/packages/orchestra/foundation/dashboard/index.blade.php
@@ -2,22 +2,23 @@
 
 @section('content')
 <div class="row">
-	@forelse($panes as $id => $pane)
-		<div{!! HTML::attributes(HTML::decorate($pane->get('attributes'))) !!}>
-			@if(! empty($pane->get('html')))
-			{!! $pane->get('html') !!}
-			@else
-			<div class="box box-default">
-				<div class="box-header with-border">
-					<h3 class="box-title">{!! $pane->get('title') !!}</h3>
+	<div class="twelve columns">
+		@forelse($panes as $id => $pane)
+			<div{!! HTML::attributes(HTML::decorate($pane->get('attributes'))) !!}>
+				@if(! empty($pane->get('html')))
+				{!! $pane->get('html') !!}
+				@else
+				<div class="box box-default">
+					<div class="box-header with-border">
+						<h3 class="box-title">{!! $pane->get('title') !!}</h3>
+					</div>
+					{!! $pane->get('content') !!}
+				@endif
 				</div>
-				{!! $pane->get('content') !!}
-			@endif
 			</div>
-		</div>
-	@empty
-	@include('orchestra/foundation::dashboard._welcome')
-	@endforelse
+		@empty
+        @include('orchestra/foundation::dashboard._welcome')
+		@endforelse
+	</div>
 </div>
-
 @stop
