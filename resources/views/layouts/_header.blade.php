2d0a670dd89b575c83e4a0948221d48d9eabd296
Steve Bauman <steven_bauman@outlook.com>
2015-12-10 15:06:12 -0500

    Change site author

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
 <title>@yield('title') | {{ memorize('site.name', 'IT Hub') }}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="{{ memorize('site.description', 'IT Hub') }}">
-<meta name="author" content="{{ memorize('site.author', 'FBCL') }}">
+<meta name="author" content="{{ memorize('site.author', 'Steve Bauman') }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
