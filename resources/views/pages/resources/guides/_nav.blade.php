0b67eb783d75eeeff8b895d82aedff1a5de24e5f
Steve Bauman <steven_bauman@outlook.com>
2015-12-15 14:53:49 -0500

    Rework ui to display favorites only if auth'ed

--- resources/views/pages/resources/guides/_nav.blade.php
+++ resources/views/pages/resources/guides/_nav.blade.php
@@ -1,18 +1,24 @@
 <ul class="nav navbar-left navbar-nav">
+
     <li class="{{ active()->route('resources.guides.index') }}">
         <a href="{{ route('resources.guides.index') }}">
             All Guides
         </a>
     </li>
-    <li class="{{ active()->route('resources.guides.favorites') }}">
-        <a href="{{ route('resources.guides.favorites') }}">
-            <i class="fa fa-star"></i>
-            Favorites
-        </a>
-    </li>
+
+    @if(auth()->user())
+        <li class="{{ active()->route('resources.guides.favorites') }}">
+            <a href="{{ route('resources.guides.favorites') }}">
+                <i class="fa fa-star"></i>
+                Favorites
+            </a>
+        </li>
+    @endif
+
     @can('create', App\Models\Guide::class)
-    <li><a href="{{ route('resources.guides.create') }}"><i class="fa fa-plus"></i> New Guide</a></li>
+        <li><a href="{{ route('resources.guides.create') }}"><i class="fa fa-plus"></i> New Guide</a></li>
     @endcan
+
 </ul>
 
 @include('pages.issues._search')
