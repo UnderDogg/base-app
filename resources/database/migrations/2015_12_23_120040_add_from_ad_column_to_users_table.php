b8167fe1b7e68c86993ec4e23109a671118b5ca6
Steve Bauman <steven_bauman@outlook.com>
2015-12-23 12:29:47 -0500

    Added from_ad column to users table

---
+++ resources/database/migrations/2015_12_23_120040_add_from_ad_column_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFromAdColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('from_ad')->after('reset_token')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('from_ad');
+        });
+    }
+}
