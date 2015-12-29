f7cd9843207dc163a25f9f7bfe9ef6fe6ac95fac
Steve Bauman <steven_bauman@outlook.com>
2015-12-29 16:38:02 -0500

    Use hasFilesTrait for GuideSteps instead of custom methods

--- app/Models/GuideStep.php
+++ app/Models/GuideStep.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasFilesTrait;
 use Lookitsatravis\Listify\Listify;
 
 class GuideStep extends Model
 {
-    use Listify;
+    use Listify, HasFilesTrait;
 
     /**
      * The guide steps model.
@@ -68,51 +69,12 @@ class GuideStep extends Model
     }
 
     /**
-     * The morphMany images relationship.
+     * Alias for guide step images morphMany relationship.
      *
      * @return \Illuminate\Database\Eloquent\Relations\MorphMany
      */
     public function images()
     {
-        return $this->morphMany(Upload::class, 'uploadable');
-    }
-
-    /**
-     * Creates a new file attached to the current step.
-     *
-     * @param string $name
-     * @param string $type
-     * @param int    $size
-     * @param string $path
-     *
-     * @return Upload
-     */
-    public function addFile($name, $type, $size, $path)
-    {
-        $uuid = uuid();
-
-        return $this->images()->create(compact('uuid', 'name', 'type', 'path', 'size'));
-    }
-
-    /**
-     * Finds a step image by the specified UUID.
-     *
-     * @param string $uuid
-     *
-     * @return Upload
-     */
-    public function findFile($uuid)
-    {
-        return $this->images()->where(compact('uuid'))->firstOrFail();
-    }
-
-    /**
-     * Deletes all files attached to the current step.
-     *
-     * @return int
-     */
-    public function deleteFiles()
-    {
-        return $this->images()->delete();
+        return $this->files();
     }
 }
