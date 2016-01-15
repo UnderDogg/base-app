9227a698220a4dc36698d8e627a1d8975a672e15
Steve Bauman <steven_bauman@outlook.com>
2016-01-15 11:36:19 -0500

    Added Processes job for retrieving computer processes and CPU usage

---
+++ app/Jobs/Com/Computer/Processes.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs\Com\Computer;
+
+use Stevebauman\Wmi\Schemas\Namespaces;
+
+class Processes extends ComputerJob
+{
+    /**
+     * Execute the job.
+     *
+     * @return array
+     */
+    public function handle()
+    {
+        $connection = $this->wmi->connect(Namespaces::ROOT_CIMV2);
+
+        $results = $connection
+            ->newQuery()
+            ->from('Win32_PerfFormattedData_PerfProc_Process')
+            ->get();
+
+        $processes = [];
+
+        foreach ($results as $result) {
+            $processes[$result->Name] = (int) $result->PercentProcessorTime;
+        }
+
+        if (array_key_exists('_Total', $processes)) {
+            // We'll overwrite the total percentage since it is usually
+            // inaccurate as it includes idle percentage.
+            $processes['_Total'] = array_sum(array_except($processes, ['Idle', '_Total']));
+        }
+
+        return $processes;
+    }
+}
