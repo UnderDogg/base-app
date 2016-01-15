1b89d0abd59187896355927565c3f269d1fda912
Steve Bauman <steven_bauman@outlook.com>
2016-01-15 11:36:03 -0500

    Added abstract ComputerJob for handling computer credentials for WMI

---
+++ app/Jobs/Com/Computer/ComputerJob.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Jobs\Com\Computer;
+
+use App\Models\ComputerAccess;
+use App\Models\Computer;
+use App\Jobs\Job;
+use Stevebauman\Wmi\Wmi;
+
+abstract class ComputerJob extends Job
+{
+    /**
+     * @var Computer
+     */
+    protected $computer;
+
+    /**
+     * @var Wmi
+     */
+    protected $wmi;
+
+    /**
+     * Constructor.
+     *
+     * @param Computer $computer
+     */
+    public function __construct(Computer $computer)
+    {
+        $this->computer = $computer;
+
+        list($username, $password) = $this->getCredentials();
+
+        $this->wmi = new Wmi($this->computer->name, $username, $password);
+    }
+
+    /**
+     * Returns credentials for the specified computer.
+     *
+     * @return array
+     */
+    protected function getCredentials()
+    {
+        $access = $this->computer->getWmiAccess();
+
+        $username = null;
+        $password = null;
+
+        if ($access instanceof ComputerAccess) {
+            $username = $access->getWmiUsername();
+            $password = $access->getWmiPassword();
+        }
+
+        // Check if the access record contains a username and password.
+        if (is_null($username) && is_null($password)) {
+            $prefix = 'adldap.connection_settings';
+
+            $username = config("$prefix.admin_username") . config("$prefix.account_suffix");
+            $password = config("$prefix.admin_password");
+        }
+
+        return [$username, $password];
+    }
+}
--- app/Jobs/Com/Computer/ScanDisks.php
+++ app/Jobs/Com/Computer/ScanDisks.php
@@ -4,33 +4,15 @@ namespace App\Jobs\Com\Computer;
 
 use App\Jobs\Computer\CreateDisk;
 use App\Jobs\Computer\CreateDiskRecord;
-use App\Jobs\Job;
-use App\Models\Computer;
 use App\Models\ComputerHardDisk;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Stevebauman\Wmi\ConnectionInterface;
 use Stevebauman\Wmi\Models\Variants\HardDisk;
 use Stevebauman\Wmi\Schemas\Namespaces;
-use Stevebauman\Wmi\Wmi;
 
-class ScanDisks extends Job
+class ScanDisks extends ComputerJob
 {
     use DispatchesJobs;
-
-    /**
-     * @var Computer
-     */
-    protected $computer;
-
-    /**
-     * Constructor.
-     *
-     * @param Computer $computer
-     */
-    public function __construct(Computer $computer)
-    {
-        $this->computer = $computer;
-    }
 
     /**
      * Scans the current computers hard disks and creates a record of them.
@@ -39,59 +21,42 @@ class ScanDisks extends Job
      */
     public function handle()
     {
-        // Check if the computer can be accessed by WMI.
-        if ($this->computer->getWmiAccess()) {
-            // Check if the access record contains a username and password.
-            if ($this->computer->access->wmi_username && $this->computer->access->wmi_password) {
-                $username = $this->computer->access->wmi_username;
-                $password = $this->computer->access->wmi_password;
-            } else {
-                $prefix = 'adldap.connection_settings';
+        try {
+            // Try to connect to the machine.
+            $connection = $this->wmi->connect(Namespaces::ROOT_CIMV2);
 
-                $username = config("$prefix.admin_username").config("$prefix.account_suffix");
-                $password = config("$prefix.admin_password");
-            }
+            // Check that we're connected to the machine.
+            if ($connection instanceof ConnectionInterface) {
+                // Get the machines hard disks.
+                $disks = $connection->hardDisks()->get();
 
-            // Create a new WMI instance on the computer.
-            $wmi = new Wmi($this->computer->name, $username, $password);
+                $added = [];
 
-            try {
-                // Try to connect to the machine.
-                $connection = $wmi->connect(Namespaces::ROOT_CIMV2);
+                foreach ($disks as $disk) {
+                    // Go through each disk and create a new
+                    // disk record for the current computer.
+                    if ($disk instanceof HardDisk) {
+                        // Make sure we skip drive type 5 since it's a CD-ROM drive
+                        if ($disk->getDriveType() !== 5) {
+                            $job = new CreateDisk($this->computer, $disk->getName(), $disk->getSize(), $disk->getInstallDate(), $disk->getDescription());
 
-                // Check that we're connected to the machine.
-                if ($connection instanceof ConnectionInterface) {
-                    // Get the machines hard disks.
-                    $disks = $connection->hardDisks()->get();
+                            $hd = $this->dispatch($job);
 
-                    $added = [];
-
-                    foreach ($disks as $disk) {
-                        // Go through each disk and create a new
-                        // disk record for the current computer.
-                        if ($disk instanceof HardDisk) {
-                            // Make sure we skip drive type 5 since it's a CD-ROM drive
-                            if ($disk->getDriveType() !== 5) {
-                                $job = new CreateDisk($this->computer, $disk->getName(), $disk->getSize(), $disk->getInstallDate(), $disk->getDescription());
-
-                                $hd = $this->dispatch($job);
-
-                                if ($hd instanceof ComputerHardDisk) {
-                                    // If a hard disk is successfully created, we'll
-                                    // dispatch the job to create a new disk record.
-                                    $this->dispatch(new CreateDiskRecord($hd, $disk->getFreeSpace(), $disk->getStatus()));
-                                }
-
-                                $added[] = $hd;
+                            if ($hd instanceof ComputerHardDisk) {
+                                // If a hard disk is successfully created, we'll
+                                // dispatch the job to create a new disk record.
+                                $this->dispatch(new CreateDiskRecord($hd, $disk->getFreeSpace(), $disk->getStatus()));
                             }
+
+                            $added[] = $hd;
                         }
                     }
-
-                    return $added;
                 }
-            } catch (\COM_EXCEPTION $e) {
-                //
+
+                return $added;
             }
+        } catch (\COM_EXCEPTION $e) {
+            //
         }
 
         return false;
