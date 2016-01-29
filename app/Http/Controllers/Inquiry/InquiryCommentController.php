599b457da01d5f78f722a69786375bc317583568
Steve Bauman <steven_bauman@outlook.com>
2016-01-29 10:54:35 -0500

    Working on inquiry comments

---
+++ app/Http/Controllers/Inquiry/InquiryCommentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Inquiry;
+
+use App\Http\Controllers\Controller;
+
+class InquiryCommentController extends Controller
+{
+    public function store($inquiryId)
+    {
+        //
+    }
+
+    public function edit($inquiryId, $commentId)
+    {
+        //
+    }
+
+    public function update($inquiryId, $commentId)
+    {
+        //
+    }
+
+    public function destroy($inquiryId, $commentId)
+    {
+        //
+    }
+}
---
+++ app/Processors/Inquiry/InquiryCommentProcessor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Processors\Inquiry;
+
+use App\Processors\Processor;
+
+class InquiryCommentProcessor extends Processor
+{
+    public function __construct()
+    {
+        //
+    }
+}
