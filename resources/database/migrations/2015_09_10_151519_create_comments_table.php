aa18f83c9415572e53d55ef022cb164e11a0fc6e
Steve Bauman <steven_bauman@outlook.com>
2016-02-01 16:23:37 -0500

    Fixed dependencies, added PHP 7 testing

--- resources/database/migrations/2015_09_10_151519_create_comments_table.php
+++ resources/database/migrations/2015_09_10_151519_create_comments_table.php
@@ -15,6 +15,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+
             $table->integer('user_id')->unsigned();
             $table->text('content');
 
