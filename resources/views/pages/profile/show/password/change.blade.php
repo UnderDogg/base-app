21b67dc0653ecf46602d4ebb9cd955d7387e591a
Steve Bauman <steven_bauman@outlook.com>
2016-02-05 15:44:35 -0500

    Added notice if account is from active directory

--- resources/views/pages/profile/show/password/change.blade.php
+++ resources/views/pages/profile/show/password/change.blade.php
@@ -6,6 +6,21 @@
 
 @section('show.panel.body')
 
+    @if(auth()->user()->from_ad)
+        <div class="panel panel-warning">
+            <div class="panel-heading">
+                Changing Your Password
+            </div>
+            <div class="panel-body">
+                <p>
+                    Your account is from Active Directory.
+                    <br><br>
+                    Changing your password can take up to one to two hours to complete.
+                </p>
+            </div>
+        </div>
+    @endif
+
     <div class="col-md-12">
         {!! $form !!}
     </div>
