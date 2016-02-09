839d80659dd09e888da67e901bfb81b285bdd09e
Steve Bauman <steven_bauman@outlook.com>
2016-02-08 22:39:28 -0500

    Added missing property

--- app/Http/Controllers/ActiveDirectory/ComputerController.php
+++ app/Http/Controllers/ActiveDirectory/ComputerController.php
@@ -11,6 +11,11 @@ use Illuminate\Http\Request;
 class ComputerController extends Controller
 {
     /**
+     * @var ComputerProcessor
+     */
+    protected $processor;
+
+    /**
      * Constructor.
      *
      * @param ComputerProcessor $processor
