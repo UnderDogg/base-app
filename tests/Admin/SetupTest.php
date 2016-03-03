451dd9a57c468758d1ab289eb480ffddea1f2817
Steve Bauman <steven_bauman@outlook.com>
2016-03-02 23:38:33 -0500

    Remove failing test

--- tests/Admin/SetupTest.php
+++ tests/Admin/SetupTest.php
@@ -23,10 +23,21 @@ class SetupTest extends TestCase
         $this->assertEquals(403, $response->getStatusCode());
     }
 
-    public function test_begin_setup()
+    public function test_begin_setup_from_welcome()
     {
         $this->visit(route('admin.setup.welcome'))
             ->click('Begin')
             ->seePageIs(route('admin.setup.begin'));
+    }
+
+    public function test_begin_setup()
+    {
+        $this->visit(route('admin.setup.begin'))
+            ->type('Admin', 'name')
+            ->type('test@email.com', 'email')
+            ->type('Password123', 'password')
+            ->type('Password123', 'password_confirmation')
+            ->submitForm('Complete Setup')
+            ->assertResponseOk();
     }
 }
