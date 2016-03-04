c8eba43d1abb49183a6d6354139b727e8e771198
Steve Bauman <steven_bauman@outlook.com>
2016-03-04 13:45:14 -0500

    Added issue comment attachment controller

---
+++ app/Http/Controllers/Issue/IssueCommentAttachmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Issue;
+
+use App\Http\Controllers\Controller;
+
+class IssueCommentAttachmentController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+}
