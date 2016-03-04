2f257eaa3cf6de4cd4224499e13480babc8d850b
Steve Bauman <steven_bauman@outlook.com>
2016-03-04 08:48:50 -0500

    Test travis-ci

--- tests/Issue/IssueCommentTest.php
+++ tests/Issue/IssueCommentTest.php
@@ -31,7 +31,7 @@ class IssueCommentTest extends TestCase
         $user = $this->createAdmin();
 
         $this->actingAs($user);
-
+        dd($user->can('issues.comments.create'));
         $issue = factory(Issue::class)->create();
 
         $response = $this->call('POST', route('issues.comments.store', [$issue->getKey()]), [
