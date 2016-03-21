7676bd9f6f120f361dbd0ad964c70dc869f5d790
Steve Bauman <steven_bauman@outlook.com>
2016-03-21 10:16:42 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/Device/Computer/ComputerPatchController.php
+++ app/Http/Controllers/Device/Computer/ComputerPatchController.php
@@ -34,7 +34,8 @@ class ComputerPatchController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -45,7 +46,8 @@ class ComputerPatchController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -56,7 +58,8 @@ class ComputerPatchController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -67,8 +70,9 @@ class ComputerPatchController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -79,7 +83,8 @@ class ComputerPatchController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
