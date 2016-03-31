6ad27a40cb534ff4c2011cffd2b2e69799ae6913
Steve Bauman <steven_bauman@outlook.com>
2016-03-31 13:31:23 -0400

    Remove custom font

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,6 @@ elixir(function(mix) {
         'libs/select2-bootstrap.css',
         'libs/bootstrap-datetimepicker.min.css',
         'libs/bootplus.min.css',
-        'libs/montserrat.css',
         'app.css'
     ])
         .copy('resources/assets/fonts/libs/font-awesome/', 'public/fonts')
