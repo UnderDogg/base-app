cc00ffc513bc44146e0b81593202b2085fde23cb
Steve Bauman <steven_bauman@outlook.com>
2016-04-05 13:24:58 -0400

    Added large welcome buttons

--- resources/views/pages/welcome/index.blade.php
+++ resources/views/pages/welcome/index.blade.php
@@ -4,6 +4,45 @@
 
 @section('content')
 
+    @if(auth()->check())
+
+        <div class="row">
+
+            <div class="col-md-4">
+
+                <a href="{{ route('issues.create') }}" class="btn btn-success btn-xl btn-block">
+                    <i class="fa fa-ticket"></i>
+                    Create a New Ticket
+                </a>
+
+            </div>
+
+            <div class="col-md-4">
+
+                <a href="{{ route('inquiries.start') }}" class="btn btn-info btn-xl btn-block">
+                    <i class="fa fa-question-circle"></i>
+                    Create a New Request
+                </a>
+
+            </div>
+
+            <div class="col-md-4">
+
+                <a href="{{ route('resources.guides.index') }}" class="btn btn-default btn-xl btn-block">
+                    <i class="fa fa-bookmark"></i>
+                    View Guides
+                </a>
+
+            </div>
+
+            <div class="clearfix"></div>
+
+        </div>
+
+        <hr>
+
+    @endif
+
     <div class="row" id="panels">
 
         <div class="col-md-8 col-md-offset-2">
