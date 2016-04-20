3e8066cab58ba103fa582e01ecc720677d68e2b4
Steve Bauman <steven_bauman@outlook.com>
2016-04-20 12:45:44 -0400

    Added chrome theme color

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,6 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#2C3E50">
 <meta name="description" content="Helpdesk">
 <meta name="author" content="Steve Bauman">
 <meta name="csrf-token" content="{{ csrf_token() }}">
