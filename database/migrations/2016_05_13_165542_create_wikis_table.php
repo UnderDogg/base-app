85d897076d755cee21082535e793ed4d35e83671
Steve Bauman <steven_bauman@outlook.com>
2016-05-13 17:01:55 -0400

    Added user ID.

--- database/migrations/2016_05_13_165542_create_wikis_table.php
+++ database/migrations/2016_05_13_165542_create_wikis_table.php
@@ -15,9 +15,12 @@ class CreateWikisTable extends Migration
         Schema::create('wikis', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->integer('user_id')->unsigned();
             $table->string('title');
             $table->string('slug');
             $table->text('content')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
